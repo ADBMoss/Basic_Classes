13b795d1696a85b91d5cc80f5aac98b5c433b823
DarriusB <darrius.bradford@gmail.com>
2022-05-02 08:37:16 -0700

    Basic Classes Exercise Finished

---
+++ Basic_Classes/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Basic_Classes
+{
+    public class Car
+    {
+        public Car()
+        {
+        }
+
+        public object Make { get; set; }
+        public object Model { get; set; }
+        public object Year { get; set; }
+    }
+}
---
+++ Basic_Classes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Basic_Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var car1 = new Car();
+            car1.Make = "Toyota";
+            car1.Model = "Camry";
+            car1.Year = 2008;
+
+            Console.WriteLine($"Your car is a {car1.Year} {car1.Make} {car1.Model}");
+
+
+
+
+        }
+    }
+}
